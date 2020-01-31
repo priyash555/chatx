dbe46db7b837fcdfd92d2956efa9f8ae50864246
priyash555 <31501889+priyash555@users.noreply.github.com>
2020-01-31 22:02:47 +0530

    first commit

---
+++ cfehome/routing.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+
+from chat.consumers import ChatConsumer, TaskConsumer
+application = ProtocolTypeRouter({ 
+    # Websocket chat handler
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                [
+                    # url(r"chat/", TaskConsumer, name='chat'),
+                    url(r"messages/(?P<username>[\w.@+-]+)/$", ChatConsumer, name='chat')
+                ]
+            )
+        ),
+    ),
+    'channel': ChannelNameRouter({
+        'task': TaskConsumer
+    })
+})
